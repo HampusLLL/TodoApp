5479c58522107fef44db23885682d2e3e9811129
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2023-12-22 18:02:38 +0100

    Remove Todos Reassure Data Is Removed Correctly Api Test

--- TodoMVC.Core.Tests/TodoAppControllerTest.cs
+++ TodoMVC.Core.Tests/TodoAppControllerTest.cs
@@ -11,23 +11,28 @@ namespace TodoMVC.Core.Tests
     {
         private TodoDBContext _context;
 
-        // Common setup method to initialize the context
         private TodoDBContext CreateInitializedContext()
         {
             var options = new DbContextOptionsBuilder<TodoDBContext>()
-                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .UseInMemoryDatabase(databaseName: GetRandomDatabaseName())
                 .Options;
 
             var context = new TodoDBContext(options);
 
             return context;
         }
+
+        // Helper method to generate a random database name using Guid
+        private string GetRandomDatabaseName()
+        {
+            return $"TestDatabase_{Guid.NewGuid()}";
+        }
+
         [Fact]
-        public async Task Add_Notes_Reassure_Data_Is_Correct_Api_Test()
+        public async Task Add_Todos_Reassure_Data_Is_Correct_Api_Test()
         {
             // Arrange
             var _context = CreateInitializedContext();
-
             _context.Notes.Add(new Notes { ID = 1, text = "Test", isDone = false });
             _context.Notes.Add(new Notes { ID = 2, text = "Another Test", isDone = true });
             _context.Notes.Add(new Notes { ID = 3, text = "Yet Another Test", isDone = false });
@@ -46,6 +51,32 @@ namespace TodoMVC.Core.Tests
             Assert.Contains(notes, n => n.ID == 1 && n.text == "Test" && n.isDone == false);
             Assert.Contains(notes, n => n.ID == 2 && n.text == "Another Test" && n.isDone == true);
             Assert.Contains(notes, n => n.ID == 3 && n.text == "Yet Another Test" && n.isDone == false);
+            Assert.Equal(notes.Count, 3);
+        }
+
+        [Fact]
+        public async Task Remove_Todos_Reassure_Data_Is_Removed_Correctly_Api_Test()
+        {
+            // Arrange
+            var _context = CreateInitializedContext();
+            var newNote = new Notes { ID = 1, text = "Test", isDone = false };
+            _context.Notes.Add(newNote);
+            _context.Notes.Add(new Notes { ID = 2, text = "Another Test", isDone = true });
+            _context.Notes.Add(new Notes { ID = 3, text = "Yet Another Test", isDone = false });
+            _context.SaveChanges();
+
+            var controller = new TodoController(_context);
+
+            // Act
+            await controller.Delete(1);
+
+            // Assert
+            var remainingNotes = _context.Notes.ToList();
+
+            Assert.DoesNotContain(remainingNotes, n => n.ID == 1 && n.text == "Test" && n.isDone == false);
+            Assert.Contains(remainingNotes, n => n.ID == 2 && n.text == "Another Test" && n.isDone == true);
+            Assert.Contains(remainingNotes, n => n.ID == 3 && n.text == "Yet Another Test" && n.isDone == false);
+            Assert.Equal(2, remainingNotes.Count);
         }
     }
 }
